49ea527c7ebd5aae34ec456f1c4d3d4246855936
dhrvrm <48381412+dhrvrm@users.noreply.github.com>
2023-12-28 20:55:26 +0530

    File Structure Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "./_root/Pages";
+import RootLayout from "./_root/RootLayout";
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+
 import "./globals.css";
 
 function App() {
   return (
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
-
-
 
 export default App;
